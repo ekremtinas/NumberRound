c87a4cf6c5fbf7f8b55a58b536661d9073e1a387
parts-soft <info@parts-soft.nl>
2022-05-20 11:57:42 +0300

    Hatalar giderildi.

--- index.js
+++ index.js
@@ -6,17 +6,25 @@ const OnlyNumber=(value)=>{
     }
    
    }
-   
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+}
 const NumberRound=(value)=>{
    
-     value=OnlyNumber(value);
+    // value=OnlyNumber(value);
      if(!!value){
-     let v=String(value);
-     v = v==="0" ? "0.00" : v;
-     return Number(v.split(".")[1])===0 ? Math.round(v):Number(v).toFixed(2)
+     if(typeof value=="string"){
+     let v = String(value);
+     v = v === "0" ? "0.00" : v;
+     return Number(Number(v.split(".")[1])===0 ? Math.round(v):Number(v).toFixed(2))
+     }else if(typeof value == "number"){
+      value=OnlyNumber(value);
+      let v = String(value);
+      v = v === "0" ? "0.00" : v;
+      return Number(Number(v.split(".")[1])===0 ? Math.round(v):isFloat(value) ? Number(v).toFixed(2):Number(v))
+     }
      }else{
        return 0;
      }
    }
-
 module.exports = NumberRound;
