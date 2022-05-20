5d99d20cd1d5d923491499d90fafca578767eafd
parts-soft <info@parts-soft.nl>
2022-05-20 12:09:07 +0300

    Fixed değiştirilebilir hale geldi

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const OnlyNumber=(value)=>{
 function isFloat(n){
     return Number(n) === n && n % 1 !== 0;
 }
-const NumberRound=(value)=>{
+const NumberRound=(value,fixed=2)=>{
    
     // value=OnlyNumber(value);
      if(!!value){
      if(typeof value=="string"){
      let v = String(value);
      v = v === "0" ? "0.00" : v;
-     return Number(Number(v.split(".")[1])===0 ? Math.round(v):Number(v).toFixed(2))
+     return Number(Number(v.split(".")[1])===0 ? Math.round(v):Number(v).toFixed(fixed))
      }else if(typeof value == "number"){
       value=OnlyNumber(value);
       let v = String(value);
       v = v === "0" ? "0.00" : v;
-      return Number(Number(v.split(".")[1])===0 ? Math.round(v):isFloat(value) ? Number(v).toFixed(2):Number(v))
+      return Number(Number(v.split(".")[1])===0 ? Math.round(v):isFloat(value) ? Number(v).toFixed(fixed):Number(v))
      }
      }else{
        return 0;
