675c1f2b3e6dc7ec5c87bcc0c6b94895b1932ff1
ekremtinas <ekremtinas@gmail.com>
2022-09-19 15:31:21 +0300

    NumberRound tamamen değiştirildi.İşleme algoritması javascript'e göre hazırlandı.

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const OnlyNumber=(value)=>{
-    try {
-      return parseFloat(value);
-    }catch(e){
-      return 0;
-    }
-   }
-function isFloat(n){//This is isfloat function
-    return Number(n) === n && n % 1 !== 0;
-}
+
 const NumberRound=(value,fixed=2)=>{//This is main function
-     if(!!value){
-     if(typeof value=="string"){
-     let v = String(value);
-     v = v === "0" ? "0.00" : v;
-     return Number(Number(v.split(".")[1])===0 ? Math.round(v):Number(v).toFixed(fixed))
-     }else if(typeof value == "number"){
-      value=OnlyNumber(value);
-      let v = String(value);
-      v = v === "0" ? "0.00" : v;
-      return Number(Number(v.split(".")[1])===0 ? Math.round(v):isFloat(value) ? Number(v).toFixed(fixed):Number(v))
-     }
-     }else{
-       return 0;
-     }
-   }
+  let fixedValue=!!fixed ? Math.pow(10,fixed) :Math.pow(10,2);
+  if(!!value){
+  value=Number(value);
+  return  Number(Math.round(value*fixedValue)/fixedValue);
+  }else{
+    return 0;
+  }
+}
 module.exports = NumberRound;
